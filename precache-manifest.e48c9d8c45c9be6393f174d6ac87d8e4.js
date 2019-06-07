c5be2779e6ad22f7ee1f5b19e6b17c84413533ef
Tyler <tylerkrett@live.com>
2019-06-07 15:52:24 +0100

    Updates

---
+++ precache-manifest.e48c9d8c45c9be6393f174d6ac87d8e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f5e839bc24582972920",
+    "url": "/npm-module/static/js/runtime~main.55603ffe.js"
+  },
+  {
+    "revision": "9802ac27759db42a9d5c",
+    "url": "/npm-module/static/js/main.36b89e01.chunk.js"
+  },
+  {
+    "revision": "d34a74a057c74aff93ec",
+    "url": "/npm-module/static/js/2.f22ef685.chunk.js"
+  },
+  {
+    "revision": "9802ac27759db42a9d5c",
+    "url": "/npm-module/static/css/main.2b889425.chunk.css"
+  },
+  {
+    "revision": "0c33b67b479aa6a463020fbad83c30ca",
+    "url": "/npm-module/index.html"
+  }
+];
